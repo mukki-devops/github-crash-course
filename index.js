8eacc60d93995d69dc3bea5ff95a3c4d53419821
Mukesh <37791698+mukki-devops@users.noreply.github.com>
2024-01-24 22:33:11 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,2 @@
-new branch select and changes start 
-new feature add ( login work )
-new feature A
-
-now users (B) create new branch Breanch name is here B
-
-and details pushed here 
+name = stash testing 
+lets clone remote to local 
